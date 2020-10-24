93c6a09a0182be8309c29f0636357c84536f2368
Masoud <hosseiny998@gmail.com>
2020-10-24 17:24:35 +0330

    return edited

--- upload file function.php
+++ upload file function.php
@@ -1,8 +1,8 @@
 <?php
 public function uploadFile($request,$requestName,$filePath){
-        $file = $request->file($requestName);
-        $imageNewName = rand() . '.' . $file->getClientOriginalExtension();
-        $file->move(public_path($filePath),$imageNewName);
-        return $filePath;
-    }
+       $file = $request->file($requestName);
+       $imageNewName = rand() . '.' . $file->getClientOriginalExtension();
+       $storeFile = $file->move(public_path($filePath),$imageNewName);
+       if ($storeFile) return true;
+   }
 ?>
